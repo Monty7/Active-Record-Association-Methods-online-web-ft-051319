fd19c2c4f1eb3bc1d0d1c75dd916dca7f016b6ec
M Banks <montensia1@outlook.com>
2019-06-24 17:35:12 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,6 +15,9 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    binding.pry
+
+    self.artists.name.each do |artist|
+      binding.pry
+    end
   end
 end
